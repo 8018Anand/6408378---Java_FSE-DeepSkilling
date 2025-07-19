5e4b5b64755bab3d3387b062375a8975c546f1d4
Anand kumar padakala <141320444+8018Anand@users.noreply.github.com>
2025-07-19 11:44:49 +0530

    Week-5 hands on upload

---
+++ Week-5/codes/accounts/main/java/com/cognizant/account/controller/AccountController.java
@@ -0,0 +1,24 @@
+package com.cognizant.account.controller;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/accounts/")
+public class AccountController {
+	@GetMapping("{number}/")
+	public ResponseEntity<?> getEmployeeByNumber(@PathVariable String number) {
+	    Map<String, Object> response = new HashMap<>();
+	    response.put("number", "00987987973432");
+	    response.put("type", "savings");
+	    response.put("balance", 234343);
+
+	    return ResponseEntity.ok(response);
+	}	
+}
